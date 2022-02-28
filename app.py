31413861b9860a7c112c8b96e17e4f139bdbc33f
Chelsey Martinez <martinezchelsey12@gmail.com>
2022-02-28 14:50:17 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from app import create_app
+
+app = create_app()
+
+
+# Build an e-commerce application with the following things to be included:
+
+# - A user should be able to sign up 
+# - A route that shows a list of all available products
+# - When you click on a product it should be able to link to a route which shows a single product (with the information of the product you just clicked)
+# - User should be able to add a product to their cart, but only if they are logged in
+#         (you will want to build a cart table that links items in the cart with the user who owns the cart)
+# - A route (cart) that shows a list of products you’ve added into your cart as well as the total of all the items in your cart
+# - Add a route that, when clicked handles functionality that removes all items from your cart at one time. Also create a button that, when pressed, it removes that specific product object from the cart.
+
+
+# Bonus: Deploy to Heroku
